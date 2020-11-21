79e542b29bfc8cb10ceaccb4bf96a9a0fc0329c7
Sherlin Toriz Pérez <sherlintp09@gmail.com>
2020-11-20 21:16:41 -0600

    324 y correcion de error :(

--- src/app/services/carga-imagenes.service.ts
+++ src/app/services/carga-imagenes.service.ts
@@ -14,7 +14,36 @@ export class CargaImagenesService {
   constructor( private db: AngularFirestore ) { }
 
   cargarImagenesFirebase(imagenes: FileItem[]){
-    console.log ( imagenes );
+
+    const storageRef = firebase.storage().ref();
+
+      for(const item of imagenes){
+          item.estaSubiendo = true;
+          if (item.progreso >= 100) {
+            continue;
+          }
+
+          const uploadTask: firebase.storage.UploadTask = 
+            storageRef.child(`${ this.CARPETA_IMAGENES }/${ item.nombreArchivo }`)
+                      .put(item.archivo)
+
+                      uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED,
+                        (snapshot) => item.progreso = (snapshot.bytesTransferred / snapshot.totalBytes) * 100,
+                        (error) => console.error('Error al subir', error),
+                        () => {
+                          console.log('Imagen cargada correctamente');
+                 
+                          uploadTask.snapshot.ref.getDownloadURL().then(res => {
+                            item.url = res;
+                            item.estaSubiendo = false;
+                            this.guardarImagen({
+                              nombre: item.nombreArchivo,
+                              url: item.url
+                            });
+                          });
+                        }
+                      );
+          }
   }
 
 
